079972fc4b7365f483d06c17e201723c2a6dad9d
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-07-13 22:45:30 -0600

    Lista creada

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<string> Productos =new List<string>();
+
+int opcion;
+do
+{
+    Console.Clear();    
+
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine(""); 
+
+
+    Console.WriteLine("Seleccione una opcion: ");
+    opcion=Convert.ToInt32(Console.ReadLine());
+
+} while(opcion!=7);

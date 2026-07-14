69f6063ebb6fe63a40baebdd11beafb56d13214b
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-07-14 15:50:49 -0600

    Lista 2

---
+++ 2/Program.cs
@@ -0,0 +1,9 @@
+﻿List<string> ciudadesT=new List<string>();
+int opcion;
+
+do
+{
+    Console.WriteLine("Ingrese una opcion: ");
+    opcion = Convert.ToInt32(Console.ReadLine());
+
+} while (opcion != 7);
